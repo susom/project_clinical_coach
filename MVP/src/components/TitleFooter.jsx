cb94fe37c88f94f38ba62fe239763d6ccc7b0d90
Irvin Szeto <irvinszeto@gmail.com>
2025-05-07 14:54:33 -0700

    adding logo to title view  and updated copyright stuff

--- MVP/src/components/TitleFooter.jsx
+++ MVP/src/components/TitleFooter.jsx
@@ -9,7 +9,7 @@ function TitleFooter() {
                 <a href="#settings">Settings</a>
             </nav>
             <p className="title-footer-copyright">
-                Clinical Coach © 2025
+                AI Clinical Coach © Stanford University, 2025
             </p>
         </footer>
     );
